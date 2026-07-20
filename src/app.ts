0ba172f9eca77791feab0ddc06ea32160c091948
Kolawole-kehinde <kolawoledhikrullah93@gmail.com>
2026-07-20 16:16:10 +0100

    add: health route

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,9 @@
 // entry file for express, express related logic
 import express from 'express'
+import { errorHandler } from './middlewares/errorHandler.js'
+import { notFound } from './middlewares/notFound.js'
+import cors from 'cors'
+import { apiRouter } from './routes/index.js'
 
 
 
@@ -7,6 +11,14 @@ export function createApp(){
     const app = express()
   
     app.use(express.json())
+    app.use(express.urlencoded({extended: true}))
     
+    app.use(cors())
+    app.use("/api", apiRouter)
+    app.use(notFound)
+    app.use(errorHandler)
+
+    return app
+
 }
 
--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,12 @@
 // our main root file
 
+import { createApp } from "./app.js";
+import { env } from "./config/env.js";
+import { logger } from "./lib/logger.js";
 
+const app = createApp();
+
+app.listen(env.port, () => {
+    logger.info(`server is now runno=ing on port http://localhost:${env.port}`);
+})
+ 
