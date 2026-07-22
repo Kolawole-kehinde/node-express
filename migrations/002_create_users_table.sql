01a48563cb8d56246a4636b7c47c7e0ed745c9ef
Kolawole-kehinde <kolawoledhikrullah93@gmail.com>
2026-07-22 11:03:06 +0100

    add: users table

---
+++ migrations/002_create_users_table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE users (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    email VARCHAR(255) NOT NOT UNIQUE,
+    password_hash TEXT,
+    google_id VARCHAR(255) UNIQUE,
+    role VARCHAR(20) NOT NULL DEFAULT 'USER'
+      CHECK (ROLE IN ('USER', ADMIN)),
+    created_at TIMESTAMPS NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPS NOT NULL DEFAULT NOW(),
+
+)
