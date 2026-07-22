b0d047c4245a2e73c687995d729d22983c948644
Kolawole-kehinde <kolawoledhikrullah93@gmail.com>
2026-07-22 11:09:36 +0100

    add: support_tasks table

---
+++ migrations/003_create_support_tasks_table.sql
@@ -0,0 +1,9 @@
+ CREATE TABLE support_tasks (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    title VARCHAR(20) NOT NULL,
+    status VARCHAR(20) NOT NULL DEFAULT 'OPEN'
+      CHECK (status IN ('OPEN', 'IN_PROGRESS', 'RESOLVED')),
+     created_at TIMESTAMPS NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPS NOT NULL DEFAULT NOW()
+ )
