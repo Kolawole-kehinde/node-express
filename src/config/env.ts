eda015bc0359533af38d820d9b71afc22139803b
Kolawole-kehinde <kolawoledhikrullah93@gmail.com>
2026-07-22 11:28:35 +0100

    add: db connection pool

--- src/config/env.ts
+++ src/config/env.ts
@@ -2,9 +2,19 @@ import dotenv from "dotenv"
 
 dotenv.config()
 
+function checkRequiredEnvVariables(key: string){
+   const value = process.env[key]
+   if(!value){
+     throw new Error(`missing env variable for ${key}`)
+   }
+
+   return value
+}
+
 export const env = {
     port: Number(process.env.PORT ?? 5000),
     isProduction:(process.env.NODE_ENV ?? "development") === "production",
     nodeEnv: process.env.NODE_ENV ?? "development",
     logLevel: process.env.LOG_LEVEL ?? "info",
+    database_url: checkRequiredEnvVariables('DATABASE_URL')
 } as const
